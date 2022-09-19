9501b714dfe5b5becb2b41e40197e953a3cb8c0e
Richard <richardmixihe@gmail.com>
2022-09-19 16:35:26 +0800

    Update index.js

--- 01-图书管理/lib/index.js
+++ 01-图书管理/lib/index.js
@@ -41,6 +41,34 @@ axios.defaults.baseURL = 'http://www.itcbc.com:3006';
 //     console.log(res);
 // })
 
+
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    NProgress.start();
+    console.log('发送咯');
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+
+  // 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    NProgress.done();
+
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
+
+
+
 const appkey = 'Richard2'
 // ------------------------获取图书并展示-------------------------------
 function renderBooks() {
@@ -126,6 +154,13 @@ document.querySelector('tbody'). addEventListener('click',function (e) {
 
 
 
-
-
 // ------------------------修改图书-------------------------------
+
+document.querySelector('#editBtn').addEventListener('click',function () {
+    const data = val(document.querySelector('#editForm'))
+    data.appkey = appkey
+    axios.put('/api/updatebook',data).then(({data:res})=>{
+        editModal.hide();
+        renderBooks();
+    })
+})//绑定事件
